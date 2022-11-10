3d3d9ea6bf6ddd7d24a0f05364d0e0056fae1c64
alpal <>
2022-11-10 16:29:07 +0000

    Update main.py to reflect changes to newer versions of iminuit

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
 # %%
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from iminuit import Minuit, cost
+from scipy.stats import expon, skewnorm
 
-from scipy.stats import skewnorm
-from iminuit import Minuit
-
-from nll import NLL
+# from nll import NLL
 
 # %%
 
+x_range = (0, 5)
+
 # data generating function
-data = skewnorm.rvs(4, size=1000)
+x_signal = skewnorm.rvs(a=1.5, loc=2, scale=1/2, size=1000)
+x_noise  = expon.rvs(scale=3, size=(len(x_signal)))
+
+x_data = np.append(x_signal, x_noise)
+x_data = x_data[(x_range[0] < x_data) & (x_data < x_range[1])]
+
+freq, edges = np.histogram(x_data, bins=50)
+x_centre = 0.5 * (edges[1:] + edges[:-1])
+x_diff = np.diff(x_centre)
+
+# %%
+
+def pdf(x, z, a, loc, scale, expscale):
+    """Custom probability density function.
+    
+    This must be normalised, which is achieved by the weight, z.
+    """
+    return z * skewnorm.pdf(x, a, loc, scale) + (1 - z) * expon.pdf(x, scale=expscale)
+
+def cdf(x, z, a, loc, scale, expscale):
+    """Custom cumulative distribution function."""
+    return z * skewnorm.cdf(x, a, loc, scale) + (1 - z) * expon.cdf(x, scale=expscale)
+
+# %%
 
 # hypothesis
-params = ("a", "loc", "scale")
+params = ('z', 'a', 'loc', 'scale', 'expscale')
 
-nll = NLL(skewnorm.pdf, data)
+# the negative log-likelihood is now built in
+cost = cost.UnbinnedNLL(x_data, pdf) # NLL(data, pdf)
+# binned fits are more computationally efficient and numerically stable for large datasets
+# cost = cost.ExtendedBinnedNLL(freq, edges, cdf)
+
 # specify initial values here
-m = Minuit(nll, a=0, loc=0, scale=1, name=params)
+m = Minuit(cost, z=0.5, a=0, loc=0, scale=1, expscale=1, name=params)
+
+# specify limits here
+m.limits['z']        = (0, 1)
+m.limits['scale']    = (0, None)
+m.limits['expscale'] = (0, None)
 
 # %%
 
 print("Parameter Values and Errors:\n", m.migrad(), "\n")
+print("Valid:\n", m.valid, "\n")
 print("Correlation Matrix:\n", m.covariance.correlation(), "\n")
 
 print("Final Values:")
 for param in params:
-    print("{} = {:.3f} +- {:.3f}".format(param, m.values[param], m.errors[param]))
+    print(f"{param} = {m.values[param]:.3f} +- {m.errors[param]:.3f}")
 
 # exclude the null with a hypothesis test of the alternate
 # calculate systematic errors along the function by finding the largest shift
@@ -36,16 +70,16 @@ for param in params:
 # %%
 
 # plot the fit
-bins = 100
-limits = (-1.5, 4.5)
+plt.errorbar(x_centre, freq, freq**0.5, fmt='ok')
 
-x = np.linspace(limits[0], limits[1])
+x_plot = np.linspace(*x_range, len(x_data))
 
-# normalise the  histogram
-plt.hist(data, bins, limits, True)
-plt.plot(x, skewnorm.pdf(x, *m.values))
+plt.plot(x_plot, pdf(x_plot, *[p.value for p in m.init_params]) * len(x_data) * x_diff[0], ls=':', label='Initial')
+plt.plot(x_plot, pdf(x_plot, *m.values) * len(x_data) * x_diff[0],                                 label='Fit')
 
-plt.title("Histogram Fit")
-plt.ylabel("Frequency")
+# plt.stairs(np.diff(cdf(edges, *[p.value for p in m.init_params])) * len(x_data), edges, ls=':', label='Initial')
+# plt.stairs(np.diff(cdf(edges, *m.values)) * len(x_data),                         edges,         label='Fit')
+
+plt.legend()
 
 plt.show()
